f486c91fec1cf38312ed6e213a85caf543dc9ff8
Timothy <materialisimo@mail.ru>
2021-10-13 22:19:59 +0300

    Update CoinFlipper.py

--- CoinFlipper.py
+++ CoinFlipper.py
@@ -20,46 +20,135 @@ pygame.display.set_caption("coinFlipper")
 carryOn = True
 
 clock = pygame.time.Clock()
+color = GREEN
+
+x = -63
+y = 365
+
+drawMiniRect = 0
 
 # Background image
-bg = pygame.image.load("bg.jpg")
+bg = pygame.image.load("images/bg.jpg")
+screen.blit(bg, (0, 0))
 
+# Game Title
+myFont = pygame.font.SysFont('Comic Sans MS', 50)
+textSurface = myFont.render('Coin Flipper!', False, (235, 235, 235))
+screen.blit(textSurface, (210, 0))
+
+# Coin info
+secondText = pygame.font.SysFont('Comic Sans MS', 40)
+secondTextSurface = secondText.render('RED = head', False, (235, 235, 235))
+screen.blit(secondTextSurface, (466, 185))
+
+# Coin info 2
+thirdText = pygame.font.SysFont('Comic Sans MS', 40)
+thirdTextSurface = thirdText.render('BLUE = tail', False, (235, 235, 235))
+screen.blit(thirdTextSurface, (466, 245))
+
+# Stats info
+fourthText = pygame.font.SysFont('Comic Sans MS', 40)
+fourthTextSurface = fourthText.render('History:', False, (235, 235, 235))
+screen.blit(fourthTextSurface, (5, 300))
+
+# Info rect
+rect = pygame.Surface((1000,750))
+rect.set_alpha(128)
+rect.fill((255, 255, 255))
+screen.blit(rect, (2, 360))
+
+# main loop
 while carryOn:
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             carryOn = False
 
-    # Game Title
-    myFont = pygame.font.SysFont('Comic Sans MS', 50)
-    textSurface = myFont.render('Coin Flipper!', False, (0, 0, 0))
-    screen.blit(textSurface, (210, 0))
+        if (event.type == pygame.MOUSEBUTTONDOWN and event.button == 1): #or (event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE):
+            pos = event.pos
 
-    # Info field
-    rect = pygame.draw.rect(screen, (BLACK), (2, 360, 695, 138), 2)
-    pygame.display.update()
+            #click for flip
+            if 250 < pos[0] < 450 and 150 < pos[1] < 350:
+                flip = random.randint(0, 1)
+                if color in [RED, BLUE]:
+                    color = GREEN
 
-    # Coin Square
-    coin = pygame.draw.rect(screen, GREEN, [250, 150, 200, 200], 0)
+                # flip
+                elif flip == 0:
+                    # coin = pygame.draw.rect(screen, RED, [250, 150, 200, 200], 0)
+                    color = RED
+                    drawMiniRect = 1
+                else:
+                    # coin = pygame.draw.rect(screen, BLUE, [250, 150, 200, 200], 0)
+                    color = BLUE
+                    drawMiniRect = 1
 
-    # Реализовать исчезновение текста при нажатии пробела и появлении нового текста говорящей о выпавшей стороне монеты.
+    # rect color info
+    if drawMiniRect == 1:
+        x += 70
+        pygame.draw.rect(screen, color, [x, y, 50, 50], 0)
+        drawMiniRect = 0
 
-    # Coin flip
-    key = pygame.key.get_pressed()
-    if key[pygame.K_SPACE]:
-        flip = random.randint(0, 1)
+        if x >= 700:
+            x = -63
+            y += 70
 
-        if flip == 0:
-            coin = pygame.draw.rect(screen, RED, [250, 150, 200, 200], 0)
+        if y >= 500:
+            x = -63
+            y = 365
 
-        else:
-            coin = pygame.draw.rect(screen, BLUE, [250, 150, 200, 200], 0)
+            screen.blit(bg, (0, 0))
 
-    # Реализовать появление квадрата определенного цвета в зависимости от выпавшей стороны в пустом блоке "rect" так,
-    # чтобы при каждом следующем подкидывании рядом с первым квадратом появлялся второй квадрат и так далее.
+            # Game Title
+            myFont = pygame.font.SysFont('Comic Sans MS', 50)
+            textSurface = myFont.render('Coin Flipper!', False, (235, 235, 235))
+            screen.blit(textSurface, (210, 0))
+
+            # Coin info
+            secondText = pygame.font.SysFont('Comic Sans MS', 40)
+            secondTextSurface = secondText.render('RED = head', False, (235, 235, 235))
+            screen.blit(secondTextSurface, (466, 185))
+
+            # Coin info 2
+            thirdText = pygame.font.SysFont('Comic Sans MS', 40)
+            thirdTextSurface = thirdText.render('BLUE = tail', False, (235, 235, 235))
+            screen.blit(thirdTextSurface, (466, 245))
+
+            # Stats info
+            fourthText = pygame.font.SysFont('Comic Sans MS', 40)
+            fourthTextSurface = fourthText.render('History:', False, (235, 235, 235))
+            screen.blit(fourthTextSurface, (5, 300))
+
+            # Info field
+            rect = pygame.Surface((1000, 750))
+            rect.set_alpha(128)
+            rect.fill((255, 255, 255))
+            screen.blit(rect, (2, 360))
+
+    # coin
+    pygame.draw.rect(screen, color, [250, 150, 200, 200], 0)
 
     # Text inside of the first square
-    firstTextSize = pygame.font.SysFont('Comic Sans MS', 30)
-    firstText = screen.blit(firstTextSize.render('Press Space', True, (0, 0, 0)), (265, 215))
+    firstTextSize = pygame.font.SysFont('Comic Sans MS', 40)
+    firstText = screen.blit(firstTextSize.render('click here', True, (0, 0, 0)), (256, 215))
+
+
+
+    pygame.display.update()
+
+    '''
+        # Coin flip
+        key = pygame.key.get_pressed()
+        if key[pygame.K_SPACE]:
+            flip = random.randint(0, 1)
+
+            if flip == 0:
+                coin = pygame.draw.rect(screen, RED, [250, 150, 200, 200], 0)
+
+            else:
+                coin = pygame.draw.rect(screen, BLUE, [250, 150, 200, 200], 0)
+
+    '''
 
     pygame.display.update()
 
